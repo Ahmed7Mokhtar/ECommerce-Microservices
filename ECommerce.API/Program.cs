77c1bf6aab879f55df99f5555c4f14032f6c9165
Ahmed Mokhtar <rooneya250@gmail.com>
2025-01-02 18:27:03 +0200

    Added CORS and Fluent Validation to Swagger Docs

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -4,6 +4,7 @@ using ECommerce.API.Middlewares;
 using System.Text.Json.Serialization;
 using ECommerce.Core.Mappers;
 using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,20 @@ builder.Services.AddCore();
 builder.Services.AddAutoMapper(typeof(AppUserMappingProfile).Assembly);
 
 builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationRulesToSwagger();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddDefaultPolicy(builderr =>
+    {
+        builderr.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 // Build the web application
 var app = builder.Build();
@@ -29,6 +44,11 @@ app.UseExceptionHandlingMiddleware();
 
 // Routing
 app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors();
 
 // Auth
 app.UseAuthentication();
